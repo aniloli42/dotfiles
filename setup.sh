b79402322efa9827ad5a2d3dfc516f4144662306
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-11-26 17:55:06 +0545

    feat: setup stage ways script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,24 @@
+#!/bin/bash
+
+stage="$1"
 echo "Setuping configs..."
 
-echo "Symlinking: git"
-stow -R git
+function symlinking () {
+  echo "Symlinking: git"
+  stow -R git
 
-echo "Symlinking: bash"
-stow -R bash
+  echo "Symlinking: bash"
+  stow -R bash
 
-echo "Symlinking: apps"
-stow -R apps
+  echo "Symlinking: apps"
+  stow -R apps
+}
 
 
+if [ -z "$stage" ]
+then
+  symlinking
+
+elif [ $stage == "link" ]
+then symlinking
+fi

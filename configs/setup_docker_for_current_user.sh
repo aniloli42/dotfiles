c665b91ae659d51caa8fe6c379cf7e9c6a5ceb53
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-11-26 17:31:30 +0545

    refactor: beautify docker setup for current user

--- configs/setup_docker_for_current_user.sh
+++ configs/setup_docker_for_current_user.sh
@@ -1,4 +1,6 @@
-sudo groupadd -f docker && sleep 1 &&
-sudo usermod -aG docker $USER && sleep 1 &&
-newgrp docker && sleep 1 &&
-groups
+sudo groupadd -f docker \
+&& sleep 1 \
+&& sudo usermod -aG docker $USER \
+&& sleep 1 \
+&& newgrp docker && sleep 1 \
+&& groups

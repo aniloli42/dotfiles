96fc27265e035af07b1269883e860d44cd20fdd1
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-12-06 23:56:37 +0545

    chore: prettify config

--- configs/monitor.sh
+++ configs/monitor.sh
@@ -1,1 +1,8 @@
-xrandr --output eDP-1 --mode 1920x1080 --rate 60 --primary && xrandr --output HDMI-1 --mode 1920x1080 --rate 75 --left-of eDP-1
+xrandr \
+  --output eDP-1 \
+  --mode 1920x1080 \
+  --rate 60 --primary \
+  && xrandr --output HDMI-1 \
+  --mode 1920x1080 \
+  --rate 75 \
+  --left-of eDP-1

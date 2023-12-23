f218c217902c1aeb9895b0cfce51e5540c770827
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-12-23 23:44:31 +0545

    feat: monitor with conditions

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,11 +1,24 @@
-xrandr \
+#!/bin/bash
 
-  --output eDP-1 \
-  --mode 1920x1080 \
-  --rate 60 \
-  --primary \
+connected_monitors=$(xrandr | grep -E ' connected' | awk '{print $1}')
 
-  --output HDMI-1 \
-  --mode 1920x1080 \
-  --rate 75 \
-  --left-of eDP-1
+
+if [[ "$connected_monitors" == *"HDMI-1"* ]]; then
+  xrandr \
+    --output eDP-1 \
+    --mode 1920x1080 \
+    --rate 60 \
+    --primary \
+    --output HDMI-1 \
+    --mode 1920x1080 \
+    --rate 75 \
+    --left-of eDP-1
+else
+    xrandr \
+      --output eDP-1 \
+      --mode 1920x1080 \
+      --rate 60 \
+      --primary \
+      --output HDMI-1 \
+      --off
+fi

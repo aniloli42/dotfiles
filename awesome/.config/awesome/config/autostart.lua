ae060769496a80df57cd5505e0c653326f57db06
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2023-12-23 22:29:18 +0545

    chore: code refactor

--- awesome/.config/awesome/config/autostart.lua
+++ awesome/.config/awesome/config/autostart.lua
@@ -3,14 +3,12 @@ local autostart = require("awful").spawn.with_shell
 -- Auto Align Monitors
 autostart("~/.dotfiles/scripts/monitor.sh")
 
+
 -- Network manager
 autostart("nm-applet")
 
 -- Bluetooth Manager
 autostart("blueman-applet")
-
--- Transparent Window View
--- autostart("picom")
 
 -- {{{ Display Power Management
 autostart("xset s off")
--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,8 +1,11 @@
 xrandr \
+
   --output eDP-1 \
   --mode 1920x1080 \
-  --rate 60 --primary \
-  && xrandr --output HDMI-1 \
+  --rate 60 \
+  --primary \
+
+  --output HDMI-1 \
   --mode 1920x1080 \
   --rate 75 \
   --left-of eDP-1

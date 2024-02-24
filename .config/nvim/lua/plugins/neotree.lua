b306f4c8b43456867b5d5c988a8138333edb7c87
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-02-24 21:28:28 +0545

    chore: neotree enhance

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -10,5 +10,14 @@ return {
 		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
 		vim.keymap.set("n", "<leader>m", ":Neotree<CR>")
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>")
+
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			enable_git_status = true,
+			enable_diagnostics = true,
+			filesystem = {
+				hijack_netrw_behavior = "open_default",
+			},
+		})
 	end,
 }

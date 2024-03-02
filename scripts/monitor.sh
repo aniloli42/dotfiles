3ac3254563a93286e18bff2e154cec5f92c784c1
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-03-02 21:48:16 +0545

    chore: as per new system refactor

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -2,22 +2,22 @@
 
 connected_monitors=$(xrandr | grep -E ' connected' | awk '{print $1}')
 
-if [[ "$connected_monitors" == *"HDMI-A-0"* ]]; then
+if [[ "$connected_monitors" == *"HDMI-1"* ]]; then
   xrandr \
-    --output eDP \
+    --output eDP-1 \
     --mode 1920x1080 \
     --rate 60 \
     --primary \
-    --output HDMI-A-0 \
+    --output HDMI-1 \
     --mode 1920x1080 \
     --rate 75 \
-    --left-of eDP
+    --left-of eDP-1
 else
     xrandr \
-      --output eDP \
+      --output eDP-1 \
       --mode 1920x1080 \
       --rate 60 \
       --primary \
-      --output HDMI-A-0 \
+      --output HDMI-1 \
       --off
 fi

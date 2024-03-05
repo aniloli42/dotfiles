19a7abd796271ef4d791ee8060757d5613966a3e
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-03-05 13:30:10 +0545

    chore: set dotfiles show in neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,23 +1,27 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
-		vim.keymap.set("n", "<leader>m", ":Neotree<CR>")
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>")
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+    vim.keymap.set("n", "<leader>m", ":Neotree<CR>")
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>")
 
-		require("neo-tree").setup({
-			close_if_last_window = true,
-			enable_git_status = true,
-			enable_diagnostics = true,
-			filesystem = {
-				hijack_netrw_behavior = "open_default",
-			},
-		})
-	end,
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      enable_git_status = true,
+      enable_diagnostics = true,
+      filesystem = {
+        hijack_netrw_behavior = "open_default",
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    })
+  end,
 }

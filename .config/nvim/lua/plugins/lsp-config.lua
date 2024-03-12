74029c69013e43b922b06ac7f2123079d55bba6d
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-03-12 22:01:49 +0545

    chore: lsp config added

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -15,8 +15,6 @@ return {
           "tsserver",
           "html",
           "cssls",
-          "eslint",
-          "denols",
           "dockerls",
           "docker_compose_language_service",
           "gopls",
@@ -39,20 +37,26 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
       local lspconfig = require("lspconfig")
-      -- Lua LSP
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      -- JS/TS LSP
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      -- html LSP
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
+
+      lspconfig.lua_ls.setup({ capabilities })
+      lspconfig.tsserver.setup({ capabilities })
+      lspconfig.html.setup({ capabilities })
+      lspconfig.cssls.setup({ capabilities })
+      lspconfig.dockerls.setup({ capabilities })
+      lspconfig.eslint.setup({ capabilities })
+      lspconfig.docker_compose_language_service.setup({ capabilities })
+      lspconfig.gopls.setup({ capabilities })
+      lspconfig.grammarly.setup({ capabilities })
+      lspconfig.graphql.setup({ capabilities })
+      lspconfig.jsonls.setup({ capabilities })
+      lspconfig.marksman.setup({ capabilities })
+      lspconfig.prismals.setup({ capabilities })
+      lspconfig.pyre.setup({ capabilities })
+      lspconfig.rust_analyzer.setup({ capabilities })
+      lspconfig.taplo.setup({ capabilities })
+      lspconfig.tailwindcss.setup({ capabilities })
+      lspconfig.yamlls.setup({ capabilities })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})

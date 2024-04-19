0cd0c069891a9b40cdd95ba168ab4bd45519f23a
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-04-19 22:52:15 +0545

    refactor: organize bash line

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -6,20 +6,10 @@ external_monitor=HDMI-1
 
 if [[ "$connected_monitors" == *"$external_montior"* ]]; then
   xrandr \
-    --output $laptop_monitor \
-    --mode 1920x1080 \
-    --rate 60 \
-    --primary \
-    --output $external_monitor \
-    --mode 1920x1080 \
-    --rate 75 \
-    --left-of $laptop_monitor
+    --output $laptop_monitor --mode 1920x1080 --rate 60 --primary \
+    --output $external_monitor --mode 1920x1080 --rate 75 --left-of $laptop_monitor
 else
     xrandr \
-      --output $laptop_monitor \
-      --mode 1920x1080 \
-      --rate 60 \
-      --primary \
-      --output $external_monitor \
-      --off
+      --output $laptop_monitor --mode 1920x1080 --rate 60 --primary \
+      --output $external_monitor --off
 fi

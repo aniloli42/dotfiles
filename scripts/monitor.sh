1901ee610d25a30bbc8acb28c2df01c2496f5e7e
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-04-24 23:30:54 +0545

    fix: typo in condition

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -4,12 +4,10 @@ connected_monitors=$(xrandr | grep -E ' connected' | awk '{print $1}')
 laptop_monitor=eDP-1
 external_monitor=HDMI-1
 
-if [[ "$connected_monitors" == *"$external_montior"* ]]; then
+if [[ $connected_monitors == *$external_monitor* ]]; then
   xrandr \
     --output $laptop_monitor --mode 1920x1080 --rate 60 --primary \
     --output $external_monitor --mode 1920x1080 --rate 75 --left-of $laptop_monitor
 else
-    xrandr \
-      --output $laptop_monitor --mode 1920x1080 --rate 60 --primary \
-      --output $external_monitor --off
+    xrandr --output $external_monitor --off
 fi

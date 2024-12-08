18fad47e1653ec0f7a53f5c510cfaac7454fa920
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-12-08 13:16:42 -0600

    remove: old nvim config removed :

--- .config/nvim/lua/configs/init.lua
+++
@@ -1,3 +0,0 @@
-require("configs.vim")
-require("configs.lazy")
-require("configs.keybinds")
--- .config/nvim/lua/configs/keybinds.lua
+++
@@ -1,7 +0,0 @@
-local k = vim.keymap.set
-
--- Switching Windows
-k("n", "<leader>h", "<C-w>h") -- to left
-k("n", "<leader>l", "<C-w>l") -- to right
-k("n", "<leader>k", "<C-w>k") -- to up
-k("n", "<leader>j", "<C-w>j") -- to bottom

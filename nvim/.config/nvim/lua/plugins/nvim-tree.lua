e3d09f82de0ba72cb87c63ea3e3c93ed50d84d71
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2024-12-26 21:01:52 -0600

    feat: config nvim tree

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -12,20 +12,45 @@ return {
         width = 35,
         relativenumber = true,
       },
-        -- change folder arrow icons
-        rendered = {
-          indent_markers = {
-            enable = true,
-          },
-          icons = {
-            glyphs = {
-              folder = {
-                arrow_closed = "",
-                arrow_open = "",
-              },
+      -- change folder arrow icons
+      rendered = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "",
+              arrow_open = "",
             },
           },
         },
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      }, 
+      filters = {
+        custom = { ".DS_Store" },
+      },
+      git = {
+        ignore = false,
+      }, 
     })
+
+
+    -- set keymap
+    local k = vim.keymap
+
+    k.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    k.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file"})
+    k.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    k.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end
 }

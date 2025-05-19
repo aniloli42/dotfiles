1c8f5f7cbbf75b6cd6839b35f39d0fb10e492963
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2025-05-19 18:39:39 -0500

    chore: alpha startup setup with bini

---
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,52 @@
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    -- personal header
+    local header = {
+      "          _____                    _____                    _____                    _____          ",
+      "         /\\    \\                  /\\    \\                  /\\    \\                  /\\    \\         ",
+      "        /::\\    \\                /::\\    \\                /::\\____\\                /::\\    \\        ",
+      "       /::::\\    \\               \\:::\\    \\              /::::|   |                \\:::\\    \\       ",
+      "      /::::::\\    \\               \\:::\\    \\            /:::::|   |                 \\:::\\    \\      ",
+      "     /:::/\\:::\\    \\               \\:::\\    \\          /::::::|   |                  \\:::\\    \\     ",
+      "    /:::/__\\:::\\    \\               \\:::\\    \\        /:::/|::|   |                   \\:::\\    \\    ",
+      "   /::::\\   \\:::\\    \\              /::::\\    \\      /:::/ |::|   |                   /::::\\    \\   ",
+      "  /::::::\\   \\:::\\    \\    ____    /::::::\\    \\    /:::/  |::|   | _____    ____    /::::::\\    \\  ",
+      " /:::/\\:::\\   \\:::\\ ___\\  /\\   \\  /:::/\\:::\\    \\  /:::/   |::|   |/\\    \\  /\\   \\  /:::/\\:::\\    \\ ",
+      "/:::/__\\:::\\   \\:::|    |/::\\   \\/:::/  \\:::\\____\\/:: /    |::|   /::\\____\\/::\\   \\/:::/  \\:::\\____\\",
+      "\\:::\\   \\:::\\  /:::|____|\\:::\\  /:::/    \\::/    /\\::/    /|::|  /:::/    /\\:::\\  /:::/    \\::/    /",
+      " \\:::\\   \\:::\\/:::/    /  \\:::\\/:::/    / \\/____/  \\/____/ |::| /:::/    /  \\:::\\/:::/    / \\/____/ ",
+      "  \\:::\\   \\::::::/    /    \\::::::/    /                   |::|/:::/    /    \\::::::/    /          ",
+      "   \\:::\\   \\::::/    /      \\::::/____/                    |::::::/    /      \\::::/____/           ",
+      "    \\:::\\  /:::/    /        \\:::\\    \\                    |:::::/    /        \\:::\\    \\           ",
+      "     \\:::\\/:::/    /          \\:::\\    \\                   |::::/    /          \\:::\\    \\          ",
+      "      \\::::::/    /            \\:::\\    \\                  /:::/    /            \\:::\\    \\         ",
+      "       \\::::/    /              \\:::\\____\\                /:::/    /              \\:::\\____\\        ",
+      "        \\::/____/                \\::/    /                \\::/    /                \\::/    /        ",
+      "         ~~                       \\/____/                  \\/____/                  \\/____/         ",
+      "                                                                                                   ",
+    }
+    
+    -- set header
+    dashboard.section.header.val = header
+
+    -- set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "> New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "> Toggle File Explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "> Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "> Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("q", "> Quit", "<cmd>qa<CR>"),
+    }
+
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
+}

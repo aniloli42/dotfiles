71f52e864b512f27ddaa5b7e31d99c6f9b6f3c78
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2025-06-18 00:52:46 -0500

    chore: rename

--- awesome/.config/awesome/ui/menu.lua
+++ awesome/.config/awesome/ui/menu.lua
@@ -4,29 +4,29 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
-myawesomemenu = {
-  {
-    "hotkeys",
-    function()
-      hotkeys_popup.show_help(nil, awful.screen.focused())
-    end,
-  },
-  { "manual",      terminal .. " -e man awesome" },
-  { "edit config", editor_cmd .. " " .. awesome.conffile },
-  { "restart",     awesome.restart },
-  {
-    "quit",
-    function()
-      awesome.quit()
-    end,
-  },
+MY_AWESOME_MENU = {
+    {
+        "hotkeys",
+        function()
+            hotkeys_popup.show_help(nil, awful.screen.focused())
+        end,
+    },
+    { "manual", terminal .. " -e man awesome" },
+    { "edit config", editor_cmd .. " " .. awesome.conffile },
+    { "restart", awesome.restart },
+    {
+        "quit",
+        function()
+            awesome.quit()
+        end,
+    },
 }
 
-mymainmenu = awful.menu({
-  items = {
-    { "awesome",       myawesomemenu, beautiful.awesome_icon },
-    { "open terminal", terminal },
-  },
+MY_MAIN_MENU = awful.menu({
+    items = {
+        { "awesome", MY_AWESOME_MENU, beautiful.awesome_icon },
+        { "open terminal", terminal },
+    },
 })
 
 menubar.utils.terminal = terminal

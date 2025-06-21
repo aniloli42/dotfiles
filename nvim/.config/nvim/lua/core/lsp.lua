7f446d58e6c422c0053fdd98a050f0f7baae4e39
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2025-06-21 01:25:41 -0500

    chore: change in lsp config

--- nvim/.config/nvim/lua/core/lsp.lua
+++ nvim/.config/nvim/lua/core/lsp.lua
@@ -1,12 +1,27 @@
+vim.lsp.enable({
+  "lua_ls",
+})
+
 -- show diagnostic line
 vim.diagnostic.config({
   virtual_text = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = true,
+  },
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = " ",
       [vim.diagnostic.severity.WARN] = " ",
       [vim.diagnostic.severity.INFO] = "󰋼 ",
       [vim.diagnostic.severity.HINT] = "󰌵 ",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
     },
   },
 })

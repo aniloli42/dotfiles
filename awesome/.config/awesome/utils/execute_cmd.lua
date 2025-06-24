3cde276c4a5d3717c3a764ad07c66761116cca72
Anil Oli aniloli42 <44222164+aniloli42@users.noreply.github.com>
2025-06-24 01:26:24 -0500

    chore: cleanup for mac

--- awesome/.config/awesome/utils/execute_cmd.lua
+++
@@ -1,8 +0,0 @@
-local function execute_cmd(command)
-  local file = io.popen(command)
-  local output = file:read("*all")
-  file:close()
-  return output
-end
-
-return execute_cmd
--- scripts/setup_docker_for_current_user.sh
+++
@@ -1,6 +0,0 @@
-sudo groupadd -f docker \
-&& sleep 1 \
-&& sudo usermod -aG docker $USER \
-&& sleep 1 \
-&& newgrp docker && sleep 1 \
-&& groups
